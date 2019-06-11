4a048d8647590d43a6d02d1ea58a3411137d570f
two <bruno.c1.2@hotmail.com>
2019-06-11 17:16:56 -0300

    implementação de classes carteirinha e certificado

---
+++ app/autoload/Carteirinha.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Carteirinha{
+
+        public  $nomeAluno;
+        public  $cpfAluno;
+        public  $cursos;
+        public  $turmaId;
+        public  $regId;
+
+
+        public function __construct(){
+
+            $this->nomeAluno    =   $nomeAluno;
+            $this->cpfAluno     =   $cpfAluno;
+            $this->cursos       =   $cursos;
+            $this->turmaId      =   $turmaId;
+            $this->regId        =   $regId;
+        }
+
+    }
+
+?>
---
+++ app/autoload/Certificado.php
@@ -0,0 +1,29 @@
+<?php
+
+    class Certificado{
+
+        public  $nomeAluno;
+        public  $cpfAluno;
+        public  $curso;
+        public  $dataI;
+        public  $dataF;
+        public  $cargaHs;
+        public  $prof;
+        public  $cpfProf;
+
+        public function __construct(){
+
+            $this->nomeAluno    =   $nomeAluno;
+            $this->cpfAluno     =   $cpfAluno;
+            $this->curso        =   $curso;
+            $this->dataI        =   $dataI;
+            $this->dataF        =   $dataF;
+            $this->cargaHs      =   $cargaHs;
+            $this->prof         =   $prof;
+            $this->cpfProf      =   $cpfProf;
+
+        }
+
+    }
+
+?>
