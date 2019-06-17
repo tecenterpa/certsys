8d6b3e58f743bd540e984297b6b3844ffa130f3a
two <bruno.c1.2@hotmail.com>
2019-06-17 10:51:41 -0300

    modelo de certificado

---
+++ app/modelos/certificado.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/certificado.css">
+    <title>CERTIFICADO</title>
+</head>
+<body>
+    <div>
+        <div class="certificado">
+            <div class="curso-bandeira">
+                <span><img src="imagens/brasil.jpg"></span>
+                <h1>CERTIFICADO NACIONAL DE</h1>
+                <h1>OPERADO DE ESCAVADEIRA HIDRÁULICA</h1>            
+            </div>
+            <div class="text">
+                <center>
+                    <h2>Bruno Duarte da Silva</h2>
+                    <h3>CPF: 060.975.987-74</h3>
+                    <h4>CAPACITADO PARA OPERADOR DE ESCAVADEIRA HIDRÁULICA</h4>
+                    <h5 align="justify">
+                        Conferido ao Aluno por ter concluído o curso em conformidade com a NR - 18, item 18.22.1, realizado
+                        no dia 30 do mês de Maio a 04 do mês de Junho na Cidade de <span>GOIATUBA</span> - GO com Aulas
+                        presenciais, Campo, Prático e Estudo em Casa com duração total de 60 <span>Horas/Aulas</span>.
+                    </h5>
+                </center>
+                
+                <p align="right"><span>AUTORIZAÇÃO E VALIDADE DE CURSOS</span> conforme a lei nº. 9394/96, o Decreto nº. 5.154/04 e a Deliberação</p>
+                <p align="right">CEE 14/97 (Indicação CEE 14/97)</p>
+                <p align="right"><span>Registro Interno</span> 184.5</p>
+            </div>            
+            <div class="info">
+                <div class="linha">
+                </div>                
+            </div>
+            <div  class="text-info">
+                <span>PARA SOLICITAR 2º VIA OU CONSULTAR A AUTENTICIDADE DESDE DOCUMENTO ACESSE: WWW.MVBCURSOS.COM.BR/REGISTRO</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
